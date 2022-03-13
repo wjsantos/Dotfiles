4222601d1cf30632c28ff766f426ae742e61cedc
Wellington Santos <wjsantos.wj@gmail.com>
2022-03-13 12:05:45 -0300

    keyboard: new script draft to change keyboard layouts

--- keyboard-change.sh
+++ keyboard-change.sh
@@ -2,7 +2,6 @@
 set -eu
 
 # https://wiki.archlinux.org/title/Xorg/Keyboard_configuration
-# bt-abnt2
 
 KEYBOARD=${KEYBOARD:-us}
 MODEL=pc105
@@ -10,7 +9,7 @@ LAYOUT=us
 VARIANT=intl
 OPTS=""
 
-# set us keyboard
+# set thinkpad keyboard
 if [ "$KEYBOARD" == "thinkpad" ] ; then
   # thinkpad E14
   MODEL=thinkpad60
@@ -25,10 +24,16 @@ fi
 echo "setxkbmap -model $MODEL -layout $LAYOUT -variant $VARIANT -option $OPTS"
 
 # Cedilla
-echo "==== having Cedilla problems? Have a look on this files:"
-find /usr/lib -name immodules.cache
-echo /usr/share/X11/locale/en_US.UTF-8/Compose
-echo "sed 's/ć/ç/g' < /usr/share/X11/locale/en_US.UTF-8/Compose | sed 's/Ć/Ç/g' > Compose
-sudo mv Compose /usr/share/X11/locale/en_US.UTF-8/Compose"
+if [ $LAYOUT == "us" ] ; then
+  # http://linuxlegal.blogspot.com/2014/02/cedilha-no-ubuntu-1310-com-teclado.html
 
+  echo "==== having Cedilla problems? Have a look on this files:"
+  find /usr/lib -name immodules.cache
+  echo /usr/share/X11/locale/en_US.UTF-8/Compose
+  echo "sed 's/ć/ç/g' < /usr/share/X11/locale/en_US.UTF-8/Compose | sed 's/Ć/Ç/g' > Compose
+  sudo mv Compose /usr/share/X11/locale/en_US.UTF-8/Compose"
 
+  echo "- Check your /etc/environment for:"
+  echo "GTK_IM_MODULE=cedilla"
+  echo "QT_IM_MODULE=cedilla"
+fi
