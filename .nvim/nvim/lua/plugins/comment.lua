d88049432f8548826ef74b0284dfb03728553ee3
Wellington Santos <wjsantos.wj@gmail.com>
2023-02-28 11:53:41 -0300

    nvim: removing wrong commited folder

--- .nvim/nvim/lua/plugins/comment.lua
+++
@@ -1,7 +0,0 @@
-local setup, comment = pcall(require, "Comment")
-if not setup then return end
-
--- VISUAL MODE BINDINGS
--- `gc` - Toggles the region using linewise comment
--- `gb` - Toggles the region using blockwise comment
-comment.setup()
