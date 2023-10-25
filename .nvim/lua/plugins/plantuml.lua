1c0c4ff4051110b2ebd8c63590e4bfe90397db02
Wellington Santos <wjsantos.wj@gmail.com>
2023-10-25 18:56:56 -0300

    plantuml: hardcode plantuml jar file

--- .nvim/lua/plugins/plantuml.lua
+++ .nvim/lua/plugins/plantuml.lua
@@ -1,7 +1,10 @@
+# "sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk"
 vim.cmd([[
-  au FileType plantuml let g:plantuml_previewer#plantuml_jar_path = get(
-    \  matchlist(system('cat `which plantuml` | grep plantuml.jar'), '\v.*\s[''"]?(\S+plantuml\.jar).*'),
-    \  1,
-    \  0
-    \)
+  " au FileType plantuml let g:plantuml_previewer#plantuml_jar_path = get(
+  "   \  matchlist(system('cat `which plantuml` | grep plantuml.jar'), '\v.*\s[''"]?(\S+plantuml\.jar).*'),
+  "   \  1,
+  "   \  0
+  "   \)
+
+  let g:plantuml_previewer#plantuml_jar_path="/opt/homebrew/Cellar/plantuml/1.2023.9/libexec/plantuml.jar"
 ]])
